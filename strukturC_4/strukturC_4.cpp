21a1f4ba883632da051dea088e37f5a10b9cbe83
Bangwahyu14 <whagung553@gmail.com>
2024-04-01 15:41:17 +0700

    selesai dengan menambahkan beberapa for intdalam int main

--- strukturC_4/strukturC_4.cpp
+++ strukturC_4/strukturC_4.cpp
@@ -15,7 +15,7 @@ struct Mahasiswa {
 };
 int main() {
 	Mahasiswa mhs[3];
-	for(int i=0;i<3;i++){
+	for (int i = 0; i < 3; i++) {
 		cout << "Data ke-" << (i + 1) << ":" << endl;
 		cout << "Nomor Mahasiswa : ";
 		getline(cin, mhs[i].nim);
@@ -31,3 +31,15 @@ int main() {
 		cin >> mhs[i].umur;
 		cin.ignore(1, '\n');
 		cout << endl;
+	}
+	for (int i = 0; i < 3; i++) {
+		cout << endl;
+		cout << "Data Mahasiswa ke-" << (i + 1) << ":" << endl;
+		cout << "\n NIM :" << mhs[i].nim;
+		cout << "\n Nama :" << mhs[i].nama;
+		cout << "\n Alamat :";
+		cout << "\n \t Desa : " << mhs[i].alamat.desa;
+		cout << "\n \t Kota : " << mhs[i].alamat.kota;
+		cout << "\n Umur :" << mhs[i].umur;
+		cout << endl;
+	}
