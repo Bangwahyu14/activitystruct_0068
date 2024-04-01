b06b01bafbdadf7aa268d28149554b2700977c39
Bangwahyu14 <whagung553@gmail.com>
2024-04-01 15:20:14 +0700

    menambahkan gotline mhs.nim

---
+++ strukturC_3/strukturC_3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	DetailAlamat alamat;
+	int umur;
+};
+int main() {
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa : ";
+	gotline(cin, mhs.nim);
+	cout << "Nama Mahasiswa : ";
+	cin >> mhs.nama;
