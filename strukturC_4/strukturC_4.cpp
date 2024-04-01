869562150cf7837717bb701df2d6a2bf5f432838
Bangwahyu14 <whagung553@gmail.com>
2024-04-01 15:30:16 +0700

    menuliskan cout "data ke"

---
+++ strukturC_4/strukturC_4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	DetailAlamat alamat;
+	int umur;
+};
+int main() {
+	Mahasiswa mhs[3];
+	for(int i=0;i<3;i++){
+		cout << "Data ke-" << (i + 1) << ":" << endl;
